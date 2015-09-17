2f72db5c89724d71115ba97078eb1ebe4daf3b85
Max Hunter <nippoo@nippoo.net>
2015-09-17 18:10:17 +0100

    Apply Pierre's changes to reflect updated file format

--- make_kwik.py
+++ make_kwik.py
@@ -13,7 +13,7 @@ import shutil
 
 import numpy as np
 
-from phy.cluster.algorithms import SpikeDetekt
+from phy.detect.spikedetekt import SpikeDetekt
 from phy.electrode import load_probe
 from phy.io.h5 import open_h5
 from phy.io.kwik import create_kwik, KwikCreator, KwikModel
@@ -22,8 +22,20 @@ from phy.traces.waveform import WaveformLoader, SpikeLoader
 from phy.utils.logging import info
 
 
+filename         = '20141202_allGT.dat' #Name of the file
+basename         = '20141202_allGT'     #Directory where all the results are stored
+prb_file         = 'kenneth.prb'      #Name of the mapping file, where it is located
+n_channels       = 120                #NUmber of active channels 
+n_total_channels = 129                #Number of channels
+sample_rate      = 25000              #Sampling rate
+N_t              = int(5*1e-3*sample_rate) #length of the template in time steps
+dtype            = 'int16'
+offset           = 0
+gain             = 0.01
+
+
 def _read_spikes(basename):
-    with open_h5(basename + '.spiketimes.mat', 'r') as f:
+    with open_h5(basename + '/' + basename.split('/')[-1] + '.spiketimes.mat', 'r') as f:
         spike_samples = {}
         for name in f.children():
             cluster = int(name.split('_')[1])
@@ -41,73 +53,107 @@ def _read_spikes(basename):
         return spikes[idx], spike_clusters[idx]
 
 
-def _read_templates(basename):
-    with open_h5(basename + '.templates.mat', 'r') as f:
+def _read_templates(basename, probe, n_total_channels, n_channels):
+    with open_h5(basename + '/' + basename.split('/')[-1] + '.templates.mat', 'r') as f:
         templates = f.read('/templates')
         n_templates, n_samples, n_channels = templates.shape
-        n_templates //= 2
-        templates = templates[:n_templates, :, :]
-        M = templates.max(axis=1).max(axis=1)
-        templates /= M[:, None, None]
-        masks = templates.max(axis=1)
+        n_templates    //= 2
+        templates        = templates[:n_templates, :, :]
+        masks            = np.zeros((n_templates, n_channels))
+        electrodes       = np.argmax(np.abs(templates).max(1), 1)
+
+        inv_nodes        = np.zeros(n_total_channels, dtype=np.int32)
+        nodes            = []
+        for key in probe['channel_groups'].keys():
+          nodes += probe['channel_groups'][key]['channels']
+        nodes            = np.array(nodes, dtype=np.int32)
+        idx              = np.argsort(nodes)
+        nodes            = np.sort(nodes)
+        inv_nodes[nodes] = np.argsort(nodes)
+
+        def get_edges(i, channel_groups):
+          edges = []
+          pos_x, pos_y = channel_groups['geometry'][i]
+          for c2 in channel_groups['channels']:
+              pos_x2, pos_y2 = channel_groups['geometry'][c2]
+              if (((pos_x - pos_x2)**2 + (pos_y - pos_y2)**2) <= probe['radius']**2):
+                  edges += [c2]
+          return edges
+
+        for count, i in enumerate(electrodes):
+            for key in probe['channel_groups'].keys():
+              if nodes[i] in probe['channel_groups'][key]['channels']:
+                masks[count, idx[inv_nodes[get_edges(nodes[i], probe['channel_groups'][key])]]] = 1
     return templates, masks
 
 
-def _truncate(fn, offset=None, n_channels=None, itemsize=None):
+def _truncate(fn, extension='.dat', offset=None, n_channels=None, itemsize=None, dtype=None, chunk_size=50000):
     """Eventually truncate a file at the end to ensure it has a correct shape.
     """
-    n_bytes = os.stat(fn).st_size
-    n_samples = (n_bytes - offset) // (itemsize * n_channels)
-    n_bytes_final = offset + n_samples * n_channels * itemsize
-    trunc = n_bytes - n_bytes_final
-    assert n_bytes_final <= n_bytes
-    assert trunc >= 0
+    data = np.memmap(fn, dtype=dtype, offset=offset)
+    N    = data.shape[0]
+    
+    if np.mod(N, n_channels) != 0:
+        
+        fn_copy   = fn + extension
+        N         = int(N/n_channels)
 
-    if trunc > 0:
-        fn_copy = fn + '.dat'
         if op.exists(fn_copy):
-            return (n_samples, n_channels)
+            return fn_copy, (N, n_channels)
 
         # Create the end-truncated file.
         info("Truncating...")
-        shutil.copy(fn, fn_copy)
-        with open(fn_copy, 'a') as f:
-            f.truncate(n_bytes_final)
-        assert os.stat(fn_copy).st_size == n_bytes_final <= n_bytes
-        info("Truncated {} bytes at the end of `{}`.".format(trunc, fn))
+        f = open(fn_copy, 'w')
+        chunk_len = n_channels*chunk_size
+        n_samples = N/chunk_len
+        for i in xrange(n_samples):
+            data = np.memmap(fn, dtype=dtype, offset=offset)
+            f.write(data[i*chunk_len:(i+1)*chunk_len])
+        f.close()
+    else:
+        fn_copy   = fn
+        N         = int(N/n_channels)
+    return fn_copy, (N, n_channels)
 
-    return (n_samples, n_channels)
 
-
-def _read_filtered(basename, n_channels=None, dtype=None):
-    fn = basename + '.filtered'
+def _read_filtered(filename, n_channels=None, dtype=None):
+    fn = filename
     with open(fn, 'rb') as f:
         data = f.read(4096)
     data = data.decode('ascii',  'ignore')
-    i = data.index('EOH')
-    # OFFSET = HEADER + EOH (3 bytes) + 2 uint16 samples (4 bytes)
-    offset = i + 3 + 2 * 2
+    try:
+        i = data.index('EOH')
+        # OFFSET = HEADER + EOH (3 bytes) + 2 uint16 samples (4 bytes)
+        offset = i + 3 + 2 * 2
+    except Exception:
+        offset = 0
     info("Header: {} bytes.".format(offset))
     dtype = np.dtype(dtype)
-    shape = _truncate(fn,
+    filename, shape = _truncate(fn,
                       offset=offset,
                       n_channels=n_channels,
-                      itemsize=dtype.itemsize)
-    return np.memmap(fn + '.dat', dtype=dtype, offset=offset, shape=shape)
+                      itemsize=dtype.itemsize, 
+                      dtype=dtype)
+    return filename, np.memmap(filename, dtype=dtype, offset=0, shape=shape)
 
 
 class Converter(object):
     def __init__(self,
                  basename,
+                 filename,
+                 N_t,
                  n_channels=None,
+                 n_total_channels=None,
                  prb_file=None,
                  dtype=None,
                  sample_rate=None,
+                 offset=0,
+                 gain=0.01
                  ):
 
         self.n_features_per_channel = 3
-        extract_s_before = 50
-        extract_s_after = 80
+        self.n_total_channels = n_total_channels
+        extract_s_before = extract_s_after = int(N_t - 1)/2
 
         self.basename = basename
         self.kwik_path = basename + '.kwik'
@@ -128,12 +174,13 @@ class Converter(object):
         self.n_samples_w = extract_s_before + extract_s_after
 
         # A xxx.filtered.trunc file may be created if needed.
-        self.traces_f = _read_filtered(basename,
-                                       n_channels=n_channels,
+        self.file, self.traces_f = _read_filtered(filename,
+                                       n_channels=n_total_channels,
                                        dtype=dtype,
                                        )
-        self.n_samples, self.n_channels = self.traces_f.shape
-        assert n_channels == self.n_channels
+        self.n_samples, self.n_total_channels = self.traces_f.shape
+        self.n_channels = n_channels
+        assert n_total_channels == self.n_total_channels
         info("Loaded traces: {}.".format(self.traces_f.shape))
 
         # Load spikes.
@@ -144,20 +191,23 @@ class Converter(object):
 
         # Chunks when computing features.
         self.chunk_size = 2500
-        self.n_chunks = int(np.ceil(self.n_spikes / self.chunk_size))
+        self.n_chunks   = int(np.ceil(self.n_spikes / self.chunk_size))
 
         # Load templates and masks.
-        self.templates, self.template_masks = _read_templates(basename)
+        self.templates, self.template_masks = _read_templates(basename, self.probe, self.n_total_channels, self.n_channels)
         self.n_templates = len(self.templates)
         info("Loaded templates: {}.".format(self.templates.shape))
 
         # The WaveformLoader fetches waveforms from the raw traces dynamically.
-        channels = self.probe['channel_groups'][1]['channels']
+        nodes            = []
+        for key in self.probe['channel_groups'].keys():
+          nodes += self.probe['channel_groups'][key]['channels']
+        nodes    = np.sort(np.array(nodes, dtype=np.int32))
         self._wl = WaveformLoader(traces=self.traces_f,
                                   n_samples=self.n_samples_w,
-                                  dc_offset=32767.,
-                                  scale_factor=.01,
-                                  channels=channels,
+                                  dc_offset=offset,
+                                  scale_factor=gain,
+                                  channels=nodes
                                   )
         # A virtual (n_spikes, n_samples, n_channels) array that is
         # memmapped to the filtered data file.
@@ -182,10 +232,10 @@ class Converter(object):
         clu = self.spike_clusters[::k]
         masks = self.template_masks[clu]
 
-        w, m = self.waveforms[::k], masks
+        w, m = self.waveformsk], masks
         self.pcs = self._sd.waveform_pcs(w, m)
         return self.pcs
-
+[::
     def compute_features(self):
         pr = ProgressReporter()
         pr.set_progress_message('Computing features: {progress:.1f}%.')
@@ -221,18 +271,13 @@ class Converter(object):
         # Create an empty Kwik file.
         info("Starting the conversion to Kwik...")
         create_kwik(kwik_path=self.kwik_path,
-                    raw_data_files=[self.basename + '.filtered.dat'],
+                    raw_data_files=[self.file],
                     prb_file=self.prb_file,
-                    n_channels=self.n_channels,
+                    n_channels=self.n_total_channels,
                     sample_rate=self.sample_rate,
                     dtype=self.dtype,
                     nfeatures_per_channel=self.n_features_per_channel,
                     overwrite=True,
-
-                    # Waveform parameters.
-                    waveform_filter=False,
-                    waveform_dc_offset=32767.,
-                    waveform_scale_factor=.01,
                     )
 
         # Compute PCs and features.
@@ -243,23 +288,28 @@ class Converter(object):
         # WARNING: watch out RAM usage here. We cannot use a generator because
         # the KwiKCreator only accepts lists at the moment.
         features = (f for f in self.compute_features())
-        masks = (m for m in self.compute_masks())
-
-        # Add spikes.
-        info("Adding the spikes in the kwik file.")
-        creator = KwikCreator(self.kwik_path)
-        creator.add_spikes(group=1,
-                           spike_samples=self.spike_samples,
-                           masks=masks,
-                           features=features,
-                           )
+        masks    = (m for m in self.compute_masks())
 
         # Add clusters.
+        creator = KwikCreator(self.kwik_path)
+
         info("Adding the clusters in the kwik file.")
         creator.add_clustering(group=1,
                                name='main',
                                spike_clusters=self.spike_clusters,
                                )
+
+
+        # Add spikes.
+        info("Adding the spikes in the kwik file.")
+        creator.add_spikes(group=1,
+                           spike_samples=self.spike_samples,
+                           masks=masks,
+                           features=features,
+                           n_channels = self.n_channels,
+                           n_features = 3
+                           )
+
 
         info("Kwik file successfully created!")
 
@@ -269,11 +319,18 @@ class Converter(object):
         from phy.plot.waveforms import plot_waveforms
         from vispy.app import run
 
-        p = c.probe['channel_groups'][1]['geometry']
-        positions = [p[channel] for channel in sorted(p)]
+        p         = {}
+        positions = []
+        nodes     = []
+        for key in c.probe['channel_groups'].keys():
+          p.update(c.probe['channel_groups'][key]['geometry'])
+          nodes     +=  c.probe['channel_groups'][key]['channels']
+          positions += [p[channel] for channel in c.probe['channel_groups'][key]['channels']]
+        idx       = np.argsort(nodes)
+        positions = np.array(positions)[idx]
 
         self._n = 2
-        wave = np.zeros((0, self.n_samples_w, self.n_channels))
+        wave    = np.zeros((0, self.n_samples_w, self.n_channels))
         w = plot_waveforms(channel_positions=positions,
                            waveforms=wave,
                            overlap=True,
@@ -290,7 +347,7 @@ class Converter(object):
         # Show waveforms.
         elif name == 'waveforms':
             templates = self.waveforms
-            masks = self.template_masks[self.spike_clusters]
+            masks     = self.template_masks[self.spike_clusters]
 
         @w.connect
         def on_key_press(e):
@@ -302,51 +359,41 @@ class Converter(object):
                                masks=masks[self._n],
                                )
                 elif name == 'waveforms':
-                    cluster = -1
-                    while cluster != 14:
-                        self._n += 1 if ('Shift' not in e.modifiers) else -1
-                        sample = self.spike_samples[self._n]
-                        cluster = self.spike_clusters[self._n]
+                    sample = self.spike_samples[self._n]
+                    cluster = self.spike_clusters[self._n]
                     info("Waveform {}, template={}, sample={}.".format(self._n,
                          cluster, sample))
+                    
                     wav = np.vstack((templates[self._n],
-                                     # self.templates[cluster][:-1][None, ...],
-                                     ))
+                                     self.templates[cluster][:-1][None, ...]))
+                    
                     m = np.vstack((masks[self._n],
-                                   # self.template_masks[cluster][None, ...],
-                                   ))
+                                   self.template_masks[cluster][None, ...]))
                     w.set_data(waveforms=wav,
                                masks=m,
-                               spike_clusters=[0,
-                                               # 1,
-                                               ],
+                               spike_clusters=[0, 1],
                                )
         run()
 
 
-if __name__ == '__main__':
+c = Converter(basename, filename, N_t,
+              n_channels=n_channels,
+              n_total_channels=n_total_channels,
+              offset=offset,
+              prb_file=prb_file,
+              sample_rate=sample_rate,
+              dtype=dtype,
+              gain=gain
+              )
 
-    basename = 'checkerboard'
-    prb_file = 'mea_252.prb'
-    n_channels = 252
-    sample_rate = 10000
-    dtype = 'uint16'
+# Uncomment to have a look at the templates or waveforms.
+#c.template_explorer('waveforms')  # 'waveforms' or 'templates'
+#exit()
 
-    c = Converter(basename,
-                  n_channels=n_channels,
-                  prb_file=prb_file,
-                  sample_rate=sample_rate,
-                  dtype=dtype,
-                  )
+if not os.path.exists(basename + '.kwik'):
+    # Conversion.
+    c.create_kwik()
 
-    # Uncomment to have a look at the templates or waveforms.
-    # c.template_explorer('waveforms')  # 'waveforms' or 'templates'
-    # exit()
-
-    if not os.path.exists(basename + '.kwik'):
-        # Conversion.
-        c.create_kwik()
-
-    # Try to open the kwik file after the conversion.
-    model = KwikModel(c.kwik_path)
-    model.describe()
+# Try to open the kwik file after the conversion.
+model = KwikModel(c.kwik_path)
+model.describe()
